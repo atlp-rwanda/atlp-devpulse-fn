d826f8b65d01ee89a67d8d7d10b3aa21fb9e0467
gerard1001 <rutayisiregerard2002@gmail.com>
2022-10-07 16:36:35 +0200

    Fix code climate issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const Redux = React.lazy(() => import("./components/Counter/Counter"));
+const Counter = React.lazy(() => import("./components/Counter/Counter"));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Redux />} />
+      <Route path="/test_redux" element={<Counter />} />
     </Routes>
   );
 }
