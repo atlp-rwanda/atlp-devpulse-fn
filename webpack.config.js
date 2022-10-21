b42a38d66a91828768413b2bfb804ce4cdfbfaef
GilbertUp <98816131+GilbertUp@users.noreply.github.com>
2022-10-21 19:51:32 +0200

    sidebar responsiveness

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const dotenv = require('dotenv');
 const webpack = require('webpack');
-
 module.exports = () => {
   dotenv.config();
-
   const prod = process.env.NODE_ENV === 'production';
-
   return {
     mode: prod ? 'production' : 'development',
     entry: './src/index.tsx',
@@ -23,29 +20,13 @@ module.exports = () => {
     },
     resolve: {
       fallback: {
-        assert: require.resolve('assert'),
-        buffer: require.resolve('buffer'),
-        console: require.resolve('console-browserify'),
-        constants: require.resolve('constants-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        domain: require.resolve('domain-browser'),
-        events: require.resolve('events'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        path: require.resolve('path-browserify'),
-        punycode: require.resolve('punycode'),
-        process: require.resolve('process/browser'),
-        querystring: require.resolve('querystring-es3'),
-        stream: require.resolve('stream-browserify'),
-        string_decoder: require.resolve('string_decoder'),
-        sys: require.resolve('util'),
-        timers: require.resolve('timers-browserify'),
-        tty: require.resolve('tty-browserify'),
-        url: require.resolve('url'),
-        util: require.resolve('util'),
-        vm: require.resolve('vm-browserify'),
         zlib: require.resolve('browserify-zlib'),
+        https: require.resolve("https-browserify"),
+        http: require.resolve("stream-http"),
+      },
+      alias: {
+        process: "process/browser",
+        stream: "stream-browserify"
       },
       alias: {
        process: "process/browser"
@@ -87,6 +68,9 @@ module.exports = () => {
             new webpack.ProvidePlugin({
        process: 'process/browser',
       }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+       }),
     ],
   };
 };
