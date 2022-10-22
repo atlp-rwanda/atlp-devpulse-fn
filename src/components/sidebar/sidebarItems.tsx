f90747b9549d1e8e105cb09a61f8b758bf9f1d35
gerard1001 <rutayisiregerard2002@gmail.com>
2022-10-22 14:13:02 +0200

    resolving conflicts

--- src/components/sidebar/sidebarItems.tsx
+++ src/components/sidebar/sidebarItems.tsx
@@ -1,106 +1,104 @@
 import React from "react";
-import {Icon} from "@iconify/react";
+import { Icon } from "@iconify/react";
 
 export const sidebarItems1 = [
-    {
-        path:"/dashboard",
-        icon:<Icon icon="fontisto:pie-chart-1"></Icon>,
-        title:"Dashboard"
-    },
-    {
-        path:"/organisations",
-        icon:<Icon icon="fluent:people-team-20-filled"></Icon>,
-        title:"Organisations"
-    },
-    {
-        path:"/admins",
-        icon:<Icon icon="ic:round-people"></Icon>,
-        title:"Admins"
-    },
-    {
-        path:"/domains",
-        icon:<Icon icon="akar-icons:globe"></Icon>,
-        title:"Domains"
-    },
-    {
-        path:"/trainees",
-        icon:<Icon icon="ic:round-people"></Icon>,
-        title:"Trainees"
-    },
-    {
-        path:"/attendance",
-        icon:<Icon icon="teenyicons:clipboard-tick-solid"></Icon>,
-        title:"Attendance"
-    },
-    {
-        path:"/performance",
-        icon:<Icon icon="fa6-solid:arrow-trend-up"></Icon>,
-        title:"Performance"
-    },
-    {
-        path:"/sessions",
-        icon:<Icon icon="fluent:clipboard-bullet-list-ltr-16-filled"></Icon>,
-        title:"Sessions"
-    },
-    {
-        path:"/coordinators",
-        icon:<Icon icon="fluent:people-team-20-filled"></Icon>,
-        title:"Coordinators"
-    },
-    {
-        path:"/cohorts",
-        icon:<Icon icon="fa6-solid:graduation-cap"></Icon>,
-        title:"Cohorts"
-    },
-    {
-        path:"/updatedRatings",
-        icon:<Icon icon="charm:refresh"></Icon>,
-        title:"Updated Ratings"
-    },
-    {
-        path:"/grading",
-        icon:<Icon icon="bxs:dashboard"></Icon>,
-        title:"Grading System"
-    },
-    {
-        path:"/rolesandaccess",
-        icon:<Icon icon="heroicons:key-20-solid"></Icon>,
-        title:"Roles & Access"
-    },
-    {
-        path:"/notifications",
-        icon:<Icon icon="heroicons-solid:inbox-in"></Icon>,
-        title:"Notifications",
-    },
-    {
-        path:"/calendar",
-        icon:<Icon icon="ant-design:calendar-filled"></Icon>,
-        title:"Calendar"
-    },
+  {
+    path: "/dashboard",
+    icon: <Icon icon="fontisto:pie-chart-1"></Icon>,
+    title: "Dashboard",
+  },
+  {
+    path: "/organisations",
+    icon: <Icon icon="fluent:people-team-20-filled"></Icon>,
+    title: "Organisations",
+  },
+  {
+    path: "/admins",
+    icon: <Icon icon="ic:round-people"></Icon>,
+    title: "Admins",
+  },
+  {
+    path: "/domains",
+    icon: <Icon icon="akar-icons:globe"></Icon>,
+    title: "Domains",
+  },
+  {
+    path: "/trainees",
+    icon: <Icon icon="ic:round-people"></Icon>,
+    title: "Trainees",
+  },
+  {
+    path: "/attendance",
+    icon: <Icon icon="teenyicons:clipboard-tick-solid"></Icon>,
+    title: "Attendance",
+  },
+  {
+    path: "/performance",
+    icon: <Icon icon="fa6-solid:arrow-trend-up"></Icon>,
+    title: "Performance",
+  },
+  {
+    path: "/sessions",
+    icon: <Icon icon="fluent:clipboard-bullet-list-ltr-16-filled"></Icon>,
+    title: "Sessions",
+  },
+  {
+    path: "/coordinators",
+    icon: <Icon icon="fluent:people-team-20-filled"></Icon>,
+    title: "Coordinators",
+  },
+  {
+    path: "/cohorts",
+    icon: <Icon icon="fa6-solid:graduation-cap"></Icon>,
+    title: "Cohorts",
+  },
+  {
+    path: "/updatedRatings",
+    icon: <Icon icon="charm:refresh"></Icon>,
+    title: "Updated Ratings",
+  },
+  {
+    path: "/grading",
+    icon: <Icon icon="bxs:dashboard"></Icon>,
+    title: "Grading System",
+  },
+  {
+    path: "/rolesandaccess",
+    icon: <Icon icon="heroicons:key-20-solid"></Icon>,
+    title: "Roles & Access",
+  },
+  {
+    path: "/notifications",
+    icon: <Icon icon="heroicons-solid:inbox-in"></Icon>,
+    title: "Notifications",
+  },
+  {
+    path: "/calendar",
+    icon: <Icon icon="ant-design:calendar-filled"></Icon>,
+    title: "Calendar",
+  },
 ];
 
 export const sidebarItems2 = [
-    {
-        path:"/documents",
-        icon:<Icon icon="heroicons:document-20-solid"></Icon>,
-        title:"Docs"
-    },
-    {
-        path:"/help",
-        icon:<Icon icon="nimbus:globe"></Icon>,
-        title:"Help"
-    },
-    
+  {
+    path: "/documents",
+    icon: <Icon icon="heroicons:document-20-solid"></Icon>,
+    title: "Docs",
+  },
+  {
+    path: "/help",
+    icon: <Icon icon="nimbus:globe"></Icon>,
+    title: "Help",
+  },
 ];
 
 export const sidebarItems3 = [
-    {
-        path:"/settings",
-        icon:<Icon icon="akar-icons:settings-vertical"></Icon>,
-    },
-    {
-        path:"/settings",
-        icon:<Icon icon="eva:settings-2-outline"></Icon>,
-    },
-    
+  {
+    path: "/settings",
+    icon: <Icon icon="akar-icons:settings-vertical"></Icon>,
+  },
+  {
+    path: "/settings",
+    icon: <Icon icon="eva:settings-2-outline"></Icon>,
+  },
 ];
