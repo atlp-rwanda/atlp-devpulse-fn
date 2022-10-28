5648bd062e3059a617aa0f19f165c89ebf899ae7
Epaphrodis <45901123+tysepa@users.noreply.github.com>
2022-10-28 10:55:42 +0200

    Modify webpack and solve conflict

--- webpack.config.js
+++ webpack.config.js
@@ -36,29 +36,13 @@ module.exports = () => {
     },
     resolve: {
       fallback: {
-        assert: require.resolve('assert'),
-        buffer: require.resolve('buffer'),
-        console: require.resolve('console-browserify'),
-        constants: require.resolve('constants-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        domain: require.resolve('domain-browser'),
-        events: require.resolve('events'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        path: require.resolve('path-browserify'),
-        punycode: require.resolve('punycode'),
-        process: require.resolve('process/browser'),
-        querystring: require.resolve('querystring-es3'),
-        stream: require.resolve('stream-browserify'),
-        string_decoder: require.resolve('string_decoder'),
-        sys: require.resolve('util'),
-        timers: require.resolve('timers-browserify'),
-        tty: require.resolve('tty-browserify'),
-        url: require.resolve('url'),
-        util: require.resolve('util'),
-        vm: require.resolve('vm-browserify'),
         zlib: require.resolve('browserify-zlib'),
+        https: require.resolve("https-browserify"),
+        http: require.resolve("stream-http"), 
+      },
+      alias: {
+        process: "process/browser",
+        stream: "stream-browserify"
       },
     },
     module: {
@@ -93,6 +77,9 @@ module.exports = () => {
       new webpack.ProvidePlugin({
         process: "process/browser",
       }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+       }),
     ],
   };
 };
