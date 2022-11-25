6f0e62454e8ae7883edf098ef89f3a88ff5672bc
Epaphrodis <45901123+tysepa@users.noreply.github.com>
2022-11-25 16:03:36 +0200

    Update solve conflict

--- src/pages/TraineApplicant/Trainee.tsx
+++ src/pages/TraineApplicant/Trainee.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useState, useEffect } from "react";
 import { HiDotsVertical } from "react-icons/hi";
 import * as icons from "react-icons/ai";
@@ -59,8 +58,6 @@ const AddTrainee = (props: any) => {
   }, []);
 
   const cycle = cycles.data;
-  const trainees = alltrainees.data;
-
 
   const traine = traines.message;
   useEffect(() => {
--- src/redux/actions/deletetraine.ts
+++ src/redux/actions/deletetraine.ts
@@ -11,19 +11,18 @@ import {
 import { toast } from "react-toastify";
 
 export const deletetraine = (traineId: string) => {
-    
-    return async (dispatch:Dispatch<Action>) => {
-        dispatch({
-            type: DeleteActionType.DELETE_TRAINE_LOADING
-        });
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: DeleteActionType.DELETE_TRAINE_LOADING,
+    });
 
-        try {
+    try {
+      axios
+        .post(
+          "/",
 
-            axios.post(
-                     '/',
-
-                     {
-                        query: `mutation DeleteTrainee($deleteTraineeId: ID!) {
+          {
+            query: `mutation DeleteTrainee($deleteTraineeId: ID!) {
                                 deleteTrainee(id: $deleteTraineeId) {
                                     id
                                     email
@@ -31,55 +30,62 @@ export const deletetraine = (traineId: string) => {
                                     lastName
                                 }
                                 }`,
-                        variables: {
-                               deleteTraineeId:traineId
-                            }
-                    },
-                    {
-                        headers: {
-                        'Content-Type': 'application/json'
-                        }
-                    }
-                                )
-            .then((res)=> {
-                if (res.data.data) dispatch({type: DeleteActionType.DELETE_TRAINE_SUCCESS,message:res.data.data.deleteTrainee.email});
-                if (res.data.errors) {
-                var mess
-                res.data.errors.map((b:any)=>{ mess =b.message})
-                console.log(mess)
-                dispatch({type: DeleteActionType.DELETE_TRAINE_FAIL,error:mess});
-
-                }
-                
-            })
-            .catch(err=>{
-                dispatch({
-                        type: DeleteActionType.DELETE_TRAINE_FAIL,
-                        error: err.message
-               });
-            })
-
-
-        } catch(err:any) {
+            variables: {
+              deleteTraineeId: traineId,
+            },
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          if (res.data.data)
             dispatch({
-                type: DeleteActionType.DELETE_TRAINE_FAIL,
-                error: err.message
+              type: DeleteActionType.DELETE_TRAINE_SUCCESS,
+              message: res.data.data.deleteTrainee.email,
             });
-        }
+          if (res.data.errors) {
+            var mess;
+            res.data.errors.map((b: any) => {
+              mess = b.message;
+            });
+            console.log(mess);
+            dispatch({
+              type: DeleteActionType.DELETE_TRAINE_FAIL,
+              error: mess,
+            });
+          }
+        })
+        .catch((err) => {
+          dispatch({
+            type: DeleteActionType.DELETE_TRAINE_FAIL,
+            error: err.message,
+          });
+        });
+    } catch (err: any) {
+      dispatch({
+        type: DeleteActionType.DELETE_TRAINE_FAIL,
+        error: err.message,
+      });
     }
-}
+  };
+};
 
 //soft delete a traine
 
 export const softdeletetraine = (traineId: string) => {
-    return async (dispatch: Dispatch<softAction>) => {
-        dispatch({
-            type: softDeleteActionType.softDELETE_TRAINE_LOADING
-        });
-        try {
-            await axios.post( '/',
-                     {
-                        query: `mutation SoftdeleteTrainee($input: softdeleteTrainee) {
+  return async (dispatch: Dispatch<softAction>) => {
+    dispatch({
+      type: softDeleteActionType.softDELETE_TRAINE_LOADING,
+    });
+    try {
+      await axios
+        .post(
+          "/",
+          {
+            query: `mutation SoftdeleteTrainee($input: softdeleteTrainee) {
                                     softdeleteTrainee(input: $input) {
                                         id
                                         email
@@ -87,34 +93,53 @@ export const softdeletetraine = (traineId: string) => {
                                         lastName
                                     }
                                     }`,
-                        variables: {
-                                    "input": {
-                                        "id": traineId
-                                    }
-                                    }
-                    },
-                    {
-                        headers: {
-                        'Content-Type': 'application/json'
-                        }
-                    }
-                                )
-            .then((res)=> {
-                const bbb= res.data.data.softdeleteTrainee.email +" "+ res.data.data.softdeleteTrainee.firstname
-                if (res.data.data) dispatch({type: softDeleteActionType.softDELETE_TRAINE_SUCCESS,message:bbb});
-                if (res.data.errors) {
-                     var mess
-                res.data.errors.map((b:any)=>{ mess =b.message})
-                dispatch({type: softDeleteActionType.softDELETE_TRAINE_FAIL,error:mess});
-                }
-                
-            })
-            .catch(err=>{dispatch({type: softDeleteActionType.softDELETE_TRAINE_FAIL,error: err.message});
-            })
-        } catch(err:any) {dispatch({type: softDeleteActionType.softDELETE_TRAINE_FAIL,error: err.message});}
+            variables: {
+              input: {
+                id: traineId,
+              },
+            },
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          const bbb =
+            res.data.data.softdeleteTrainee.email +
+            " " +
+            res.data.data.softdeleteTrainee.firstname;
+          if (res.data.data)
+            dispatch({
+              type: softDeleteActionType.softDELETE_TRAINE_SUCCESS,
+              message: bbb,
+            });
+          if (res.data.errors) {
+            var mess;
+            res.data.errors.map((b: any) => {
+              mess = b.message;
+            });
+            dispatch({
+              type: softDeleteActionType.softDELETE_TRAINE_FAIL,
+              error: mess,
+            });
+          }
+        })
+        .catch((err) => {
+          dispatch({
+            type: softDeleteActionType.softDELETE_TRAINE_FAIL,
+            error: err.message,
+          });
+        });
+    } catch (err: any) {
+      dispatch({
+        type: softDeleteActionType.softDELETE_TRAINE_FAIL,
+        error: err.message,
+      });
     }
   };
-
+};
 export const fetchtraine = ({ page, itemsPerPage, All }: any) => {
   return async (dispatch: Dispatch<fetchact>) => {
     try {
@@ -245,4 +270,4 @@ export const createtraine = ({ firstName, lastName, email, cycle_id }: any) => {
       dispatch({ type: fetchtrainesss.createtrainee_fail, error: err });
     }
   };
-};
+}
