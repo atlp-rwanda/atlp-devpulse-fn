7af2c673bebf32bb647545d6201f486a4738dad8
UmuhireJessie <86103578+UmuhireJessie@users.noreply.github.com>
2022-11-28 18:51:27 +0200

    added a send email model

--- src/components/CkeckBox.tsx
+++ src/components/CkeckBox.tsx
@@ -1,22 +1,25 @@
 import React, { forwardRef, useEffect, useRef } from "react";
 
-const CkeckBox = forwardRef(({ indeterminate, ...rest }: any, ref: any) => {
-  const defaultRef = useRef();
-  const resolvedRef = ref || defaultRef;
-  useEffect(() => {
-    resolvedRef.current.indeterminate = indeterminate;
-  }, [resolvedRef, indeterminate]);
+const CkeckBox = forwardRef(
+  ({ onclick, indeterminate, ...rest }: any, ref: any) => {
+    const defaultRef = useRef();
+    const resolvedRef = ref || defaultRef;
+    useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
 
-  return (
-    <div>
-      <input
-        type="checkbox"
-        ref={resolvedRef}
-        {...rest}
-        className="w-[20px] h-[20px]"
-      />
-    </div>
-  );
-});
+    return (
+      <div>
+        <input
+          type="checkbox"
+          ref={resolvedRef}
+          {...rest}
+          className="w-[20px] h-[20px]"
+          onClick={onclick}
+        />
+      </div>
+    );
+  }
+);
 
 export default CkeckBox;
