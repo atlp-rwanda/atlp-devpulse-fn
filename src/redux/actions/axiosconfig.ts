63a3d3c3d5767801d2faa73219c5f5c0609b1fe4
kareraolivier <77101006+kareraolivier@users.noreply.github.com>
2023-01-24 13:47:10 +0200

    Ft-send-bulky-email (#61)
    * new
    
    * added a send email model
    
    * email
    
    * email
    
    Co-authored-by: UmuhireJessie <86103578+UmuhireJessie@users.noreply.github.com>

--- src/redux/actions/axiosconfig.ts
+++ src/redux/actions/axiosconfig.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const token = localStorage.getItem("Token");
-console.log("axios config", process.env.BACKEND_URL);
 const config = axios.create({
   baseURL: process.env.BACKEND_URL,
 });
