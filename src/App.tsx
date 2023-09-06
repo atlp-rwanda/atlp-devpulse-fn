1c288631656b266bdaf5c52dc2ccc60a9321b014
Jean Bosco Mugiraneza <mjeanbosco19@gmail.com>
2023-09-06 14:06:22 +0200

    feature:Application form using Google Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import AppRoutes from routes/index.tsx and use it in src/App.tsx
 
-import React from "react";
-import AppRoutes from "./routes/index";
+import React from 'react';
+import AppRoutes from './routes/index';
 
 function App() {
   return <AppRoutes />;
