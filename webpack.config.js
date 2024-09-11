a56b775fbf505fd7823d412432a5c3ccf33f7af1
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-09-11 14:06:15 +0200

    Have github actions setup with necessary jobs

--- webpack.config.js
+++ webpack.config.js
@@ -4,73 +4,69 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require("path");
 const dotenv = require("dotenv");
 const webpack = require("webpack");
-
-dotenv.config();
-
-const prod = process.env.NODE_ENV === "production";
-
-module.exports = {
-  mode: prod ? "production" : "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"), 
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    compress: true,
-    allowedHosts: "all", 
-  },
-  resolve: {
-    extensions: [".js", ".json", ".tsx", ".ts", ".svg"],
-    fallback: {
-      zlib: require.resolve("browserify-zlib"),
-      https: require.resolve("https-browserify"),
-      http: require.resolve("stream-http"),
+module.exports = () => {
+  dotenv.config();
+  const prod = process.env.NODE_ENV === "production";
+  return {
+    mode: prod ? "production" : "development",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "build/"),
+      publicPath: "/",
     },
-    alias: {
-      process: "process/browser",
-      stream: "stream-browserify", 
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      compress: true,
+      allowedHosts: ["all"],
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts)$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
+    resolve: {
+      fallback: {
+        zlib: require.resolve("browserify-zlib"),
+        https: require.resolve("https-browserify"),
+        http: require.resolve("stream-http"),
       },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      alias: {
+        process: "process/browser",
+        stream: "stream-browserify",
       },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        type: 'asset/resource', 
-        generator: {
-          filename: 'assets/[name][ext]', 
+      alias: {
+        process: "process/browser",
+        stream: "stream-browserify",
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)$/,
+          exclude: /node_modules/,
+          resolve: {
+            extensions: [".js", ".json", ".tsx", ".ts", ".svg"],
+          },
+          use: "ts-loader",
         },
-      },
-    ],
-  },
-  devtool: prod ? undefined : "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"), 
-    }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env),
-    }),
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets/favicon.ico', to: 'assets/favicon.ico' }, 
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          use: ["file-loader"],
+        },
       ],
-    }),
-  ],
+    },
+    devtool: prod ? undefined : "source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env),
+      }),
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+    ],
+  };
 };
