fedc1589a81ff94dba5479dccb3942394d1c1fe1
Bahati <admin@bahati.local>
2024-09-12 14:19:31 +0200

    Feature: Adding shareable job post buttons

--- src/pages/JobPost/viewSingleJob.tsx
+++ src/pages/JobPost/viewSingleJob.tsx
@@ -89,7 +89,8 @@ const SingleJobPostDetails = (props: any) => {
                     {fetchSingleJobPostStates.data.description}
                   </p>
                 </div>
-                <div className="flex gap-4 mt-6">
+                <div className="text-white">Share Job Post</div>
+                <div className="flex fle gap-4 mt-6">
                   <button
                     onClick={shareOnTwitterDM}
                     className="flex items-center gap-2 px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-500 transition-colors"
