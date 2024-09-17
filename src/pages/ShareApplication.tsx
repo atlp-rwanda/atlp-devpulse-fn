57e1c1a887e3d49e7fac30c5acbf73243b1d2e66
uwituzeb <b.uwituze@alustudent.com>
2024-09-17 21:04:20 +0200

    fix codeclimate

--- src/pages/ShareApplication.tsx
+++ src/pages/ShareApplication.tsx
@@ -23,19 +23,13 @@ const ShareApplication = (props: any) => {
               <div className="bg-light-bg dark:bg-dark-frame-bg  min-h-screen overflow-y-hidden overflow-x-hidden flex justify-center ">
                 <div className="flex flex-col w-[60%] dark:bg-dark-tertiary mt-[7rem]  mb-[5rem] rounded-lg p-5 md:ml-0 md:w-[90%] ">
                   <div className="flex justify-center">
-                    <p className="text-white  font-semibold underline font-size-10">
-                      {fetchSingleJobPostStates?.data?.title}
-                    </p>
+                    <p className="text-white  font-semibold underline font-size-10">{fetchSingleJobPostStates?.data?.title}</p>
                   </div>
                   <div className="flex  justify-start width-[80%] ml-3 mt-5">
-                    <p className="text-white font-sans">
-                      {fetchSingleJobPostStates?.data?.description}
-                    </p>
+                    <p className="text-white font-sans">{fetchSingleJobPostStates?.data?.description}</p>
                   </div>
                   <div className="flex flex-col justify-start width-[80%] ml-5 mt-5 ">
-                    <p className="text-white  font-semibold">
-                      Here are the requirements:
-                    </p>
+                    <p className="text-white  font-semibold">Here are the requirements:</p>
                     <ul className="list-disc ml-5">
                       {fetchSingleJobPostStates?.data?.program?.requirements.map(
                         (item: any) => (
@@ -46,9 +40,7 @@ const ShareApplication = (props: any) => {
                   </div>
                   <div className="flex justify-center mt-5">
                     <Link to={`/jobPost/${id}/apply`}>
-                      <button className="flex bg-primary dark:bg-[#56C870] rounded-md py-2 px-4 text-white font-medium cursor-pointer">
-                        Apply
-                      </button>
+                      <button className="flex bg-primary dark:bg-[#56C870] rounded-md py-2 px-4 text-white font-medium cursor-pointer">Apply</button>
                     </Link>
                   </div>
                 </div>
