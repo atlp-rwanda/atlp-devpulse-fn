8f0a3d4e334ad19f707f93f9389356e6751576c9
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-09-24 17:29:15 +0200

    adding way to send email and other adjustments

--- src/utils/DownloadPdf.tsx
+++ src/utils/DownloadPdf.tsx
@@ -1,29 +1,50 @@
 import html2canvas from "html2canvas";
-import jspdf from "jspdf";
+import jsPDF from "jspdf";
 
-export const DownloadPdf = (
-  elementId,
-  filename = "Trainee-Information.pdf"
-) => {
-  const element = document.getElementById(elementId);
+export const DownloadPdf = () => {
+  const element = document.getElementById("trainee-info");
 
   if (element) {
-    html2canvas(element)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jspdf();
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = pdf.internal.pageSize.getHeight();
+ 
+    const clonedElement = element.cloneNode(true) as HTMLElement;
 
-        const imgWidth = canvas.width;
-        const imgHeight = canvas.height;
-        const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+    clonedElement.style.position = "absolute";
+    clonedElement.style.top = "-9999px";
+    document.body.appendChild(clonedElement);
 
-        pdf.addImage(imgData, "PNG", 0, 0, imgWidth * ratio, imgHeight * ratio);
-        pdf.save(filename);
-      })
-      .catch((error) => {
-        console.error("Error capturing the div as PDF:", error);
+    html2canvas(clonedElement, {
+      scale: 1.2,
+      useCORS: true,
+      logging: true,
+      backgroundColor: null,
+      imageTimeout: 0,
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png", 1.0);
+
+      const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "pt",
+        format: [canvas.width, canvas.height],
       });
+
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const padding = 20;
+
+      pdf.addImage(
+        imgData,
+        "PNG",
+        padding,
+        padding,
+        pdfWidth - padding * 2,
+        pdfHeight - padding * 2
+      );
+      pdf.save("trainee-info.pdf");
+
+      // Remove the cloned element from the DOM after download
+      document.body.removeChild(clonedElement);
+    });
+  } else {
+    console.error("Element with id 'trainee-info' not found.");
   }
 };
