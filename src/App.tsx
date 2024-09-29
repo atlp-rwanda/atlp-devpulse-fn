03f1885dcc7fb4b73a0a4423ab2bc1f949cd62fa
Bertrand SHEMA <bentopride@gmail.com>
2024-09-30 01:29:18 +0200

    Applicant Graph
    This PR add the applicant chart graph on the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import AppRoutes from './routes/index';
+import React from "react";
+import AppRoutes from "./routes/index";
 
 function App() {
   return <AppRoutes />;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import NavBar from "../components/sidebar/navHeader";
+import ApplicantChart from "../components/Dashboard/applicantChart";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="bg-[#374151] w-full h-screen">
-        <div>
-          <div className="mt-4">
-            <h1 className="text-center text-2xl font-medium text-white">Dashboard Page</h1>
+      <div className="bg-[#374151] w-full h-screen py-14 px-12">
+        <div className="w-full border border-slate-200 py-10 px-7 rounded-2xl dark:bg-dark-bg bg-white">
+          <div className="w-full flex flex-col mb-8">
+            <h2 className="text-white font-medium text-xl">
+              My overall performamce
+            </h2>
+            <p className="text-white text-sm">As of 17 June 2022, 12:00 PM</p>
           </div>
+          <ApplicantChart />
         </div>
       </div>
     </>
