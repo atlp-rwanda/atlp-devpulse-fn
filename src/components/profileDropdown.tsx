f4cd90ad29e0d03af56b34972f7dda3603209141
Philimuhire <philimuhire@gmail.com>
2024-09-30 09:06:14 +0200

    initialized a settings component

--- src/components/profileDropdown.tsx
+++ src/components/profileDropdown.tsx
@@ -37,7 +37,7 @@ function ProfileDropdown({
                   </Link>
                   <Link
                     onClick={handleShowProfileDropdown}
-                    to="settings"
+                    to="/Settings/settings"
                     className="font-semibold text-gray-600 dark:text-black  px-4 py-2 pb-4 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900"
                   >
                     <>{t("Preferences")}</>
