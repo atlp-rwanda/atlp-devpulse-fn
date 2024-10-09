646b1e9fa7592f2ad585f6e172b530d21fc64f05
Bahati <bahatiyves10@gmail.com>
2024-10-09 12:36:17 +0200

    Fixing job application process for an applicant

--- src/components/GoogleForm/SaveForm.tsx
+++ src/components/GoogleForm/SaveForm.tsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react';
-import axios from '../../redux/actions/axiosconfig';
-import { showSuccessToast, showErrorToast } from './../../utils/toast';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import InputField from './InputField';
-import SelectField from './SelectField';
+import { useEffect, useState } from "react";
+import axios from "../../redux/actions/axiosconfig";
+import { showSuccessToast, showErrorToast } from "./../../utils/toast";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import InputField from "./InputField";
+import SelectField from "./SelectField";
 
 const validationSchema = Yup.object().shape({
-  link: Yup.string().required('Please enter a Google Form link'),
-  title: Yup.string().required('Please enter a form title'),
-  jobpost: Yup.string().required('Please select a job post'),
-  description: Yup.string().required('Please enter a description'),
+  link: Yup.string().required("Please enter a Google Form link"),
+  title: Yup.string().required("Please enter a form title"),
+  jobpost: Yup.string().required("Please select a job post"),
+  spreadsheetlink: Yup.string().required("Please enter a spreadsheetlink link"),
+  formrange: Yup.string().required("Please enter formrange name"),
+  description: Yup.string().required("Please enter a description"),
 });
 
 function SaveFormDetails() {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
   const [jobposts, setJobposts] = useState<any>([]);
 
@@ -30,7 +32,7 @@ function SaveFormDetails() {
       }
       `;
       try {
-        const response = await axios.post('/', {
+        const response = await axios.post("/", {
           query: graphqlQuery,
           variables: { input: { All: true, page: 1 } },
         });
@@ -40,7 +42,7 @@ function SaveFormDetails() {
           throw new Error(response.data.errors[0].message);
         }
       } catch (err: any) {
-        console.error('Error fetching job posts:', err);
+        console.error("Error fetching job posts:", err);
         setError(err.message);
       }
     };
@@ -48,39 +50,51 @@ function SaveFormDetails() {
   }, []);
 
   const formik = useFormik({
-    initialValues: { link: '', title: '', jobpost: '', description: '' },
+    initialValues: {
+      link: "",
+      title: "",
+      jobpost: "",
+      description: "",
+      spreadsheetlink: "",
+      formrange: "",
+    },
     validationSchema,
     onSubmit: async (values) => {
       setLoading(true);
       const graphqlQuery = `
-      mutation CreateApplication($link: String!, $title: String!, $jobpost: String!, $description: String!) {
-        createApplication(link: $link, title: $title, jobpost: $jobpost, description: $description) {
-          id
-          link
-          title
-          jobpost
-          description
-        }
-      }`;
+      mutation CreateApplication($link: String!, $title: String!, $jobpost: String!, $description: String!, $spreadsheetlink: String!, $formrange: String!) {
+  createApplication(link: $link, title: $title, jobpost: $jobpost, description: $description, spreadsheetlink: $spreadsheetlink, formrange: $formrange) {
+    _id
+    link
+    title
+    jobpost
+    description
+    spreadsheetlink
+    formrange
+  }
+}
+      `;
 
       try {
-        const response = await axios.post('/', {
+        const response = await axios.post("/", {
           query: graphqlQuery,
           variables: values,
         });
+        console.log("FULL RESPONSE OBJECT =>>>>>>>>>>>>>>> ", response);
+
         if (response.data.errors) {
           const errorMessage = response.data.errors[0].error;
           if (errorMessage.toLowerCase().includes("a record with link"))
-            showErrorToast('The link is already in use');
+            showErrorToast("The link is already in use");
         } else {
           setSuccess(true);
-          showSuccessToast('Application created successfully!');
+          showSuccessToast("Application created successfully!");
           formik.resetForm();
-          window.location.href = '/#/admin/view-forms';
+          window.location.href = "/#/admin/view-forms";
         }
       } catch (err: any) {
-        console.error('Error submitting form:', err);
-        showErrorToast('An error occurred');
+        console.error("Error submitting form:", err);
+        showErrorToast("An error occurred");
       } finally {
         setLoading(false);
       }
@@ -88,20 +102,23 @@ function SaveFormDetails() {
   });
 
   return (
-    <div className='overflow-auto'>
-      <div className='relative max-w-xl mx-auto'>
-        <h2 className='text-2xl font-bold text-center text-primary dark:text-secondary sm:text-4xl'>
+    <div className="overflow-auto">
+      <div className="relative max-w-xl mx-auto">
+        <h2 className="text-2xl font-bold text-center text-primary dark:text-secondary sm:text-4xl">
           Save Application Form
         </h2>
-        <p className='mt-4 text-lg leading-6 text-center text-primary dark:text-secondary'>
+        <p className="mt-4 text-lg leading-6 text-center text-primary dark:text-secondary">
           Copy Google Form URL and paste it in the input field below.
         </p>
 
-        <form className='grid grid-cols-1 mt-10 gap-y-3 sm:grid-cols-2 sm:gap-x-8' onSubmit={formik.handleSubmit}>
+        <form
+          className="grid grid-cols-1 mt-10 gap-y-3 sm:grid-cols-2 sm:gap-x-8"
+          onSubmit={formik.handleSubmit}
+        >
           <InputField
-            id='title'
-            name='title'
-            label='Form Title'
+            id="title"
+            name="title"
+            label="Form Title"
             value={formik.values.title}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
@@ -109,53 +126,86 @@ function SaveFormDetails() {
             error={formik.errors.title}
           />
           <SelectField
-            id='jobpost'
-            name='jobpost'
-            label='Select Job Post'
+            id="jobpost"
+            name="jobpost"
+            label="Select Job Post"
             value={formik.values.jobpost}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            options={[{ value: '', label: 'Select job title' }, ...jobposts.map((job: any) => ({ value: job.id, label: job.title }))]}
+            options={[
+              { value: "", label: "Select job title" },
+              ...jobposts.map((job: any) => ({
+                value: job.id,
+                label: job.title,
+              })),
+            ]}
             touched={formik.touched.jobpost}
             error={formik.errors.jobpost}
           />
           <InputField
-            id='link'
-            name='link'
-            label='Google Form Link'
+            id="link"
+            name="link"
+            label="Google Form Link"
             value={formik.values.link}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             touched={formik.touched.link}
             error={formik.errors.link}
-            classname='w-[212%]'
+            classname="w-[212%]"
           />
-          <div className='sm:col-span-2'>
-            <label className='block text-sm font-medium text-primary dark:text-secondary'>
+          <br />
+          <InputField
+            id="spreadsheetlink"
+            name="spreadsheetlink"
+            label="Google spreadsheet Link"
+            value={formik.values.spreadsheetlink}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            touched={formik.touched.spreadsheetlink}
+            error={formik.errors.spreadsheetlink}
+            classname="w-[212%]"
+          />
+          <br />
+          <InputField
+            id="formrange"
+            name="formrange"
+            label="Form Range Name"
+            value={formik.values.formrange}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            touched={formik.touched.formrange}
+            error={formik.errors.formrange}
+            classname="w-[212%]"
+          />
+          <div className="sm:col-span-2">
+            <label className="block text-sm font-medium text-primary dark:text-secondary">
               Description
             </label>
             <textarea
-              id='description'
-              name='description'
-              className={`block w-full dark:bg-dark-tertiary rounded-md py-3 px-4 ${formik.touched.description && formik.errors.description ? 'border-red-500' : ''
-                }`}
+              id="description"
+              name="description"
+              className={`block w-full dark:bg-dark-tertiary rounded-md py-3 px-4 ${
+                formik.touched.description && formik.errors.description
+                  ? "border-red-500"
+                  : ""
+              }`}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               rows={3}
               value={formik.values.description}
-              placeholder='Enter description...'
+              placeholder="Enter description..."
             />
             {formik.touched.description && formik.errors.description && (
-              <div className='text-red-500'>{formik.errors.description}</div>
+              <div className="text-red-500">{formik.errors.description}</div>
             )}
           </div>
 
           <button
-            type='submit'
-            className='w-full inline-flex items-center justify-center rounded-md bg-primary dark:bg-[#56C870] px-6 py-3 font-medium text-white shadow-sm hover:bg-blue-700'
+            type="submit"
+            className="w-full inline-flex items-center justify-center rounded-md bg-primary dark:bg-[#56C870] px-6 py-3 font-medium text-white shadow-sm hover:bg-blue-700"
             disabled={loading}
           >
-            {loading ? 'Submitting...' : 'Save the Form'}
+            {loading ? "Submitting..." : "Save the Form"}
           </button>
         </form>
       </div>
