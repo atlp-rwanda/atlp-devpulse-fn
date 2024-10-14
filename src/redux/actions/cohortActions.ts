78b4b3b6c13b3ca7c3516f30137f48470ee7c03d
berniceu <113672733+berniceu@users.noreply.github.com>
2024-10-14 21:12:57 +0200

    #105 Ft applicant dashboard  (#197)
    * add applicant dashboard page
    
    * add dashboard cards
    
    * integrate with backend
    
    * fetch all data from their respective queries
    
    * remove console logs
    
    * update cohort types and query
    
    * fix routes
    
    * fix code climate
    
    * add graph
    
    * fix chart
    
    * delete yarn
    
    * fix graph and login
    
    * fix
    
    * trigger
    
    * revert
    
    ---------
    
    Co-authored-by: uwituzeb <b.uwituze@alustudent.com>

--- src/redux/actions/cohortActions.ts
+++ src/redux/actions/cohortActions.ts
@@ -1,7 +1,7 @@
-import creator from './creator';
-import { GET_COHORTS } from '..';
-import axios from './axiosconfig';
-import { toast } from 'react-toastify';
+import creator from "./creator";
+import { GET_COHORTS, GET_TRAINEE_COHORT } from "..";
+import axios from "./axiosconfig";
+import { toast } from "react-toastify";
 
 export const getAllCohorts = () => async (dispatch: any) => {
   try {
@@ -35,5 +35,46 @@ export const getAllCohorts = () => async (dispatch: any) => {
     return cohorts.length;
   } catch (error) {
     return 0;
+  }
+};
+
+export const getCohort =(getCohortId: any) => async (dispatch: any) => {
+  try {
+    const response= await axios.post(`${process.env.BACKEND_URL}`, {
+      query: `
+        query GetCohort($getCohortId: ID!) {
+  getCohort(id: $getCohortId) {
+    title
+    trainees
+    end
+    id
+    phase
+    start
+    cycle {
+      endDate
+      id
+      name
+      startDate
+    }
+    program {
+      _id
+      description
+      duration
+      mainObjective
+      modeOfExecution
+      requirements
+      title
+    }
+  }
+}
+      `,
+      variables: { getCohortId }
+    });
+    const cohortData = await response.data.data.getCohort;
+    dispatch(creator(GET_TRAINEE_COHORT, cohortData));
+  } catch (error) {
+    if (error) {
+      return console.log(error);
+    }
   }
 };
