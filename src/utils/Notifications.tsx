ce341a576d70ebbb872771c8af75040c2f40152b
chris <iradukundachristian202@gmail.com>
2024-10-20 12:26:22 +0200

    fix notifications.tsx

--- src/utils/Notifications.tsx
+++ src/utils/Notifications.tsx
@@ -67,7 +67,6 @@ const useNotificationsState = () => {
     toast.info(`New notification: ${notification.message}`, toastOptions);
   };
 
-  const channel = usePusherNotifications(userId, addNotification);
   useFetchNotifications(userId, setNotifications, setUnreadCount);
   usePusherNotifications(userId, addNotification);
 
