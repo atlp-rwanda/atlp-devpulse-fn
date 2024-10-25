9ef0c73264db1015b6942e1853d756e1d1f458ca
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-10-25 08:02:29 +0200

    #161 Removed changes that are not in my task description

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -1,27 +1,17 @@
-import React from "react";
+import React from 'react';
 
-const SearchResultItem = ({
-  activeItem,
-  name,
-  length,
-  results,
-  Component,
-  noResultsMessage,
+const SearchResultItem= ({ 
+  activeItem, 
+  name, 
+  length, 
+  results, 
+  Component, 
+  noResultsMessage 
 }) => {
   if (activeItem !== name) return null;
 
-  const props = {
-    Users: { users: results },
-    Roles: { roles: results },
-    Jobs: { jobs: results },
-    Cohorts: { cohorts: results },
-    Programs: { programs: results },
-    Trainees: { trainees: results },
-    Applicationcycles: { applicationCycles: results },
-  };
-
   return length > 0 ? (
-    <Component {...props[name]} />
+    <Component {...{ [name.toLowerCase()]: results }} />
   ) : (
     <p className="text-center py-6 text-white">{noResultsMessage}</p>
   );
