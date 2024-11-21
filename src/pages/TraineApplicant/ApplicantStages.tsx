10f73855bc3679ecdcec4a20a405bae142f637f4
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-11-21 14:35:16 +0200

    Fixes close button

--- src/pages/TraineApplicant/ApplicantStages.tsx
+++ src/pages/TraineApplicant/ApplicantStages.tsx
@@ -363,7 +363,7 @@ const ApplicantStages = (props: any) => {
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 title="Open ID Document"
-                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                               >
                                 ID
                               </Link>
@@ -372,7 +372,7 @@ const ApplicantStages = (props: any) => {
                               <Link
                                 to={`/view-external-document?fileUrl=${encodeURIComponent(item.resumeUrl)}&backUrl=${encodeURIComponent("/")}&title=${encodeURIComponent("Resume Document")}`}
                                 target="_blank"
-                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
+                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-green-400"
                               >
                                 Resume
                               </Link>
@@ -381,7 +381,7 @@ const ApplicantStages = (props: any) => {
                               <Link
                                 to={`/view-external-document?fileUrl=${encodeURIComponent(item.coverLetterUrl)}&backUrl=${encodeURIComponent("/")}&title=${encodeURIComponent("Cover letter Document")}`}
                                 target="_blank"
-                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-red-400"
                               >
                                 Cover letter
                               </Link>
