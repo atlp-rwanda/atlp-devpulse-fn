6bb501fdcb8d988ecc76ec05ffca6f388a42199f
SaddockAime <aimegetz@gmail.com>
2024-12-03 20:56:00 +0200

    fix(#290): fix add trainee in cohort

--- src/pages/Cohort/CohortDetailPage.tsx
+++ src/pages/Cohort/CohortDetailPage.tsx
@@ -9,6 +9,7 @@ import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { useParams } from "react-router-dom";
 import { Spinner } from "flowbite-react";
 import { CohortDetailSkeleton } from "../../skeletons/singleCohortSketon";
+import { toast } from "react-toastify";
 
 const CohortsDetailPage = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -38,10 +39,14 @@ const CohortsDetailPage = () => {
   const handleAddTraineeModal = async () => {
     setLoading(true);
     try {
-      await dispatch(getAllTraineeApplicants());
-      setAddTrainee(true);
+      const result = await dispatch(getAllTraineeApplicants());
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        setAddTrainee(true);
+      }
     } catch (error) {
-      console.error("Error adding trainee:", error);
+      toast.error("Error adding trainee");
     } finally {
       setLoading(false);
     }
